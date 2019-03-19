c85e5dbb2d5b2b8b67add2c37773140e6d12c36f
iamchriswu <18855032030@163.com>
2019-03-19 13:37:59 +0800

    update

--- micro-service-web-admin-feign/src/main/java/xyz/tostring/micro/service/web/admin/feign/service/AdminService.java
+++ micro-service-web-admin-feign/src/main/java/xyz/tostring/micro/service/web/admin/feign/service/AdminService.java
@@ -1,10 +1,12 @@
 package xyz.tostring.micro.service.web.admin.feign.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import xyz.tostring.micro.service.web.admin.feign.service.hystrix.AdminServiceHystrix;
 
+@Service
 @FeignClient(value = "micro-service-service-admin", fallback = AdminServiceHystrix.class)
 public interface AdminService {
 
